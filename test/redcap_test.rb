bf5dea508e4cfd13fb4c9d6f08ab371e832db30f
Peter Clark <peter@5clarks.net>
2017-02-18 09:59:54 -0600

    add tests

--- test/redcap_test.rb
+++ test/redcap_test.rb
@@ -1,11 +1,28 @@
 require 'test_helper'
 
 class RedcapTest < Minitest::Test
+
+  def setup
+    @redcap = Redcap.new
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Redcap::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_that_it_has_a_configuration
+    assert_instance_of Redcap::Configuration, @redcap.configuration
+  end
+
+  def test_that_host_initializes_from_env
+    assert_equal @redcap.configuration.host, ENV['REDCAP_HOST']
+  end
+
+  def test_that_token_initializes_from_env
+    assert_equal @redcap.configuration.token, ENV['REDCAP_TOKEN']
+  end
+
+  def test_that_format_defaults_to_json
+    assert_equal @redcap.configuration.format, :json
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'redcap'
+require 'redcap/configuration'
 
 require 'minitest/autorun'
